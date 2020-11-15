eb28ee0fd68548230891bc2a5ab8f9d975b04499
hyperscypion <tomasz_derek@wp.pl>
2020-11-15 01:10:51 +0100

    Added monitor class

--- autopilot/screen.py
+++ autopilot/screen.py
@@ -1,4 +1,5 @@
 import numpy as np
+from mss import mss
 from dataclasses import dataclass
 
 
@@ -18,17 +19,49 @@ class Box:
         return image[self.x0 : self.x0 + self.x1, self.y0 : self.y0 + self.y1]
 
     @property
-    def height(self):
+    def height(self) -> int:
         return self.y1 - self.y0
 
     @property
-    def width(self):
+    def width(self) -> int:
         return self.x1 - self.x0
+
+    @staticmethod
+    def from_tuple(tuple_: tuple):
+        return Box(tuple_[0], tuple_[1], tuple_[2], tuple_[3])
+
+    def return_params(self):
+        return self.y0, self.x0, self.y1, self.x1
+
+
+class Monitor:
+    def __init__(self, left: int, top: int, width: int, height: int):
+        self.left = left
+        self.top = top
+        self.width = width
+        self.height = height
+
+    @property
+    def to_dict(self):
+        return {
+            "left": self.left,
+            "top": self.top,
+            "width": self.width,
+            "height": self.height,
+        }
+
+    def grab_monitor(self):
+        return np.array(mss().grab(self.to_dict))
 
 
 class Window:
     def __init__(self):
-        pass
+        self.name = ""
+        self.width = ""
+        self.height = ""
 
-    def select_screen(self, image):
+    def from_tuple(self, image):
+        raise NotImplementedError
+
+    def from_box(self, box: Box):
         raise NotImplementedError
